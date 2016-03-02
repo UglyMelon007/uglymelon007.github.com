bb71cc762793c56cb353807b2faee76208a82584
UglyMelon007 <UglyMelon007@gmail.com>
2016-03-02 22:39:09 +0800

    template improve test

--- js/vimwiki.js
+++ js/vimwiki.js
@@ -1,4 +1,6 @@
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://"); document.write(unescape( "%3Cspan id='cnzz_stat_icon_1257740965'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol +"s95.cnzz.com/z_stat.php%3Fid%3D1257740965%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
+var theJS = 'https://uglymelon007.disqus.com/embed.js';
+$.getScript(theJS);
 //var kwiki = {
 //    loadCmt: function(provider) {
 //        var thread, extraCSS, theJS;
